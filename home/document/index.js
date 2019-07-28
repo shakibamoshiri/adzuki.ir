df0b7b84ca29fe05e313103247f478b10ffe2368
Shakiba Moshiri <hi@shakiba.it>
2019-07-28 12:33:52 +0430

    website source code

---
+++ home/document/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment, Component } from "react";
+import { render } from "react-dom";
+
+const rootJs = <Fragment>
+    <h1>document</h1>
+    <p>This is React.js part; ready to go.</p>
+</Fragment>;
+
+const rootHtml = document.getElementById( "root" );
+render( rootJs, rootHtml );
