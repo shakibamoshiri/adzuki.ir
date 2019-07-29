638b7d0ac8a39246e35df0b3ad89317dfa956b39
Shakiba Moshiri <hi@shakiba.it>
2019-07-29 15:48:34 +0430

    Add contents

--- home/technologies/index.js
+++ home/technologies/index.js
@@ -1,10 +1,64 @@
 import React, { Fragment, Component } from "react";
 import { render } from "react-dom";
+import Button from '@material-ui/core/Button';
+const log = console.log;
 
-const rootJs = <Fragment>
-    <h1>technologies</h1>
-    <p>This is React.js part; ready to go.</p>
-</Fragment>;
+const Title = ( props ) => {
+    return <h1>{ props.children }</h1>
+}
+
+const SubTitle = ( props ) => {
+    return <h3 className="sub-title">{ props.children }</h3>
+}
+
+const Page = ( props ) => {
+    return props.children;
+}
+
+const Strong = ( props ) => {
+    return <strong style={{ color: props.color }} >{ props.children }</strong>
+}
+
+const root = <Page>
+    <Title>technologies</Title>
+    <hr />
+    <p>Technologies that have been used</p>
+
+    <SubTitle>GNU/Linux</SubTitle>
+    <p>Everything has been coded and tested and is running on a Desktop or Server version of <strong>GNU/Linux</strong> but you should not face any difficulties using <Strong color="#c80000">Adzuki</Strong> on <strong>Windows</strong>.</p>
+
+    <SubTitle>Node.js</SubTitle>
+    <p>Node.js has been used as a <strong>server</strong> also as a <strong>utility</strong>.</p>
+    <p>In case of which version, version <strong>8.9.1+</strong> will be good.</p>
+
+    <SubTitle>Express.js</SubTitle>
+    <p>Express.js will take care of requests and also static files.</p>
+
+    <SubTitle>JSON</SubTitle>
+    <p>A simple JSON file has been used as the <strong>database</strong> to keep track of each path / directory.</p>
+
+    <SubTitle>React.js</SubTitle>
+    <p>The UI part can be with React.js; but anyone can change it to whatever they want.</p>
+
+    <SubTitle><a target="_blank" href="https://sass-lang.com/">Sass</a></SubTitle>
+    <p>Also for styling <strong>scss</strong> format has been used.</p>
+
+</Page>;
 
 const rootHtml = document.getElementById( "root" );
-render( rootJs, rootHtml );
+render( root, rootHtml );
+
+const footerJs = <Fragment>
+    <div className="links">
+        <a href="/"><Button size="small" variant="contained" color="primary"> home </Button></a>
+        <a href="/consideration"><Button size="small" variant="contained" color="primary"> consideration </Button></a>
+        <a href="/tools"><Button size="small" variant="contained" color="secondary"> tools </Button></a>
+        <a href="/document"><Button size="small" variant="contained" color="primary"> document </Button></a>
+        <a href="/example"><Button size="small" variant="contained" color="primary"> example </Button></a>
+        <a href="/architecture"><Button size="small" variant="contained" color="primary"> architecture </Button></a>
+        <a href="/source"><Button size="small" variant="contained" color="primary"> source </Button></a>
+    </div>
+</Fragment>;
+
+const footer = document.getElementById( "footer" );
+render( footerJs, footer );
